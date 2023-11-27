873f35387d7b9f6bc57c29bd82cb7a4fa5f58e05
RolyAri <rolyari09@gmail.com>
2023-11-27 13:40:45 -0500

    api

--- src/app/services/helper.ts
+++ src/app/services/helper.ts
@@ -1,4 +1,4 @@
 //let baseUrl = 'http://localhost:8080'
-//let baseUrl = 'https://portal-examenes-backend-production.up.railway.app';
-let baseUrl = 'http://3.144.37.121';
+let baseUrl = 'https://portal-examenes-backend-production.up.railway.app';
+//let baseUrl = 'http://3.144.37.121';
 export default baseUrl;
